d5ed8dbdf961ce9bd8785eeea08303485084b08f
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-02-18 16:04:53 -0300

    Add JWT route protection

--- api/routes/users.js
+++ api/routes/users.js
@@ -51,62 +51,62 @@ router.post('/signup', (req, res, next) => {
 
 })
 
-router.post('/login', (req,res,next)=>{
-    User.find({email: req.body.email})
-    .exec()
-    .then(user =>{
-        if(user.length < 1){
-            return res.status(401).json({
-                message: 'Auth failed'
-            })
-        }
-        bcrypt.compare(req.body.password, user[0].password, (error, result)=>{
-            if(error){
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
                 return res.status(401).json({
                     message: 'Auth failed'
                 })
             }
-            if(result){
-                const token = jwt.sign({
-                    email: user[0].email,
-                    userId: user[0]._id
-                }, 
-                process.env.JWT_KEY,
-                {
-                    expiresIn: "1h"
+            bcrypt.compare(req.body.password, user[0].password, (error, result) => {
+                if (error) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    })
+                }
+                if (result) {
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                    },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: "1h"
+                        })
+                    return res.status(200).json({
+                        message: 'Auth successful',
+                        token: token
+                    })
+                }
+                return res.status(401).json({
+                    message: 'Auth failed'
                 })
-                return res.status(200).json({
-                    message: 'Auth successful',
-                    token: token
-                })
-            }
-            return res.status(401).json({
-                message: 'Auth failed'
             })
         })
-    })
-    .catch(err =>{
-        console.log(err)
-        res.status(200).json({
-            error: err
+        .catch(err => {
+            console.log(err)
+            res.status(200).json({
+                error: err
+            })
         })
-    })
 })
 
-router.delete('/:userId', (req,res,next) =>{
-    User.remove({ _id: req.params.userId})
-    .exec()
-    .then(result => {
-        res.status(200).json({
-            messagee: 'User deleted'
+router.delete('/:userId', (req, res, next) => {
+    User.remove({ _id: req.params.userId })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                messagee: 'User deleted'
+            })
         })
-    })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({
-            error: err
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
         })
-    })
 })
 
 module.exports = router;
